709af0bfaa69b235ec9336a92fbb58989d6a1123
Ngwuta Desmond <chidi@jamit.app>
2023-11-07 13:25:45 +0100

    added needed dependencies and setup permission info in AndroidManifest.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -38,7 +40,40 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("androidx.vectordrawable:vectordrawable:1.1.0")
+
+    //navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+
+    // Application Lifecycle
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0-beta01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0-beta01")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0-beta01")
+
+    //Dagger-Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+
+    //EncryptedSharedPreferences
+    implementation("androidx.security:security-crypto:1.1.0-alpha03")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.13.2")
+    kapt("com.github.bumptech.glide:compiler:4.13.2")
+
+    // Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
